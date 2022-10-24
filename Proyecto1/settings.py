6217fecb40b4ab3c81b8fc84c8003d6c3157d428
estravagancia <webmaster@estravagancia.com>
2022-10-24 10:48:43 +0100

    Update settings.py

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5fy5z=6p$ia@%vv_-gd)g1z*_yj*cv@8qvko5bxy!m4ie&c(b='
+SECRET_KEY = 'django-insecure-5fy5z=6p$ia@%vv_-gd)g1z*_yj*cv@8qvko5bxy!m4ie&c(b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
